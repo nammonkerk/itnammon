d6d168860cc2b703981a0953388b764327598055
nammonkerk <nammonhugo@gmail.com>
2026-06-04 09:26:24 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+          Welcom to wedsite
+          <p>โดยศุภณัฐ เกริกชัยยวัน</p>
+          ยินดีตอนรับเข้าสู้
+    </div>
+  );
+}
